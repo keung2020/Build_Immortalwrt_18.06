33bb6f5842c3c0245cc55663b9b8625bbf7361bc
keung2020 <62239958+keung2020@users.noreply.github.com>
2021-10-26 23:23:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/hostname='ImmortalWrt'/hostname='BrianWrt@Build$(TZ=UTC-8 date "+%Y.%m.%d")'/g' package/base-files/files/bin/config_generate
+sed -i 's/hostname='ImmortalWrt'/hostname=BrianWRT/g' package/base-files/files/bin/config_generate
 
 # Modify the version number
 # echo "sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release" >> package/emortal/default-settings/files/zzz-default-settings
