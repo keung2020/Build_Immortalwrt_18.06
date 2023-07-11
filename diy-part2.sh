ed730d812f01e91b93caef3c89a3d151597fd5eb
keung2020 <62239958+keung2020@users.noreply.github.com>
2023-07-11 22:06:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,46 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-#Delete duplicate file
-rm -rf feeds/luci/applications/luci-app-aliddns
-rm -rf feeds/luci/applications/luci-app-autoreboot
-rm -rf feeds/luci/applications/luci-app-baidupcs-web
-rm -rf feeds/luci/applications/luci-app-beardropper
-rm -rf feeds/luci/applications/luci-app-dockerman
-rm -rf feeds/luci/applications/luci-app-fileassistant
-rm -rf feeds/luci/applications/luci-app-filebrowser
-rm -rf feeds/luci/applications/luci-app-gost
-rm -rf feeds/luci/applications/luci-app-guest-wifi
-rm -rf feeds/luci/applications/luci-app-iptvhelper
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-onliner
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-openvpn-server
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-pppoe-relay
-rm -rf feeds/luci/applications/luci-app-pppoe-server
-rm -rf feeds/luci/applications/luci-app-ramfree
-rm -rf feeds/luci/applications/luci-app-serverchan
-rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/luci/applications/luci-app-sms-tool
-rm -rf feeds/luci/applications/luci-app-socat
-rm -rf feeds/luci/applications/luci-app-softethervpn
-rm -rf feeds/luci/applications/luci-app-ssr-mudb-server
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-syncthing
-rm -rf feeds/luci/applications/luci-app-sysuh3c
-rm -rf feeds/luci/applications/luci-app-timecontrol
-rm -rf feeds/luci/applications/luci-app-udp2raw
-rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
-rm -rf feeds/luci/applications/luci-app-usb3disable
-rm -rf feeds/luci/applications/luci-app-verysync
-rm -rf feeds/luci/applications/luci-app-vssr
-rm -rf feeds/luci/applications/luci-app-watchcat
-rm -rf feeds/luci/applications/luci-app-wrtbwmon
-rm -rf feeds/luci/applications/luci-app-xlnetacc
-rm -rf feeds/luci/applications/luci-app-xunlei
-rm -rf feeds/luci/applications/luci-theme-argon
-
 # 修改openwrt登陆地址,把下面的192.168.1.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
 
